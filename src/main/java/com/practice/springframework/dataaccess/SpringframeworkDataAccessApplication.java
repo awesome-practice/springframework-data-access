c17c63ad95c058b9e508644f9865cde9b5ca11fc
GerogeLeon <leongeroge@gmail.com>
2019-06-13 17:33:30 +0800

    transfered here

---
+++ src/main/java/com/practice/springframework/dataaccess/SpringframeworkDataAccessApplication.java
@@ -0,0 +1,12 @@
+package com.practice.springframework.dataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkDataAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkDataAccessApplication.class, args);
+    }
+}
